916a4701cbf524be7e256c2a90eb0ec8517be6e3
Cameron <camrk13@gmail.com>
2017-12-12 20:18:14 -0500

    pluralize.js module

--- src/Counter/WordSummary/WordSummary.js
+++ src/Counter/WordSummary/WordSummary.js
@@ -1,12 +1,14 @@
 import React from 'react';
+const pluralize = require('pluralize')
+
 const wordSummary = function(props){
 	let message = null;
 	
 	if (props.wordCount === 1){
-		message = "There is " + props.wordCount.toLocaleString() + " word."
+		message = "There is " + props.wordCount.toLocaleString() + pluralize(' word', props.wordCount) + ".";
 	}
 	else{
-		message = "There are " + props.wordCount.toLocaleString()+ " words.";
+		message = "There are " + props.wordCount.toLocaleString() + pluralize(' word', props.wordCount) + ".";
 	}
 	
 	return (
